309042161f33b82f63e41b5c433afec1347f4827
eyoa <yoaevelyn@gmail.com>
2021-03-09 15:57:13 -0500

    add reviews table and update relation to product and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :reviews
   has_secure_password
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: { case_sensitive: false }, presence: true
